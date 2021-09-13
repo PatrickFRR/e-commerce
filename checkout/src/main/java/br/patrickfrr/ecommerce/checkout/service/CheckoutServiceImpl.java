9404067e48344f83ed60d02648863abd5d346421
PatrickFRR <patrick_ribeiro@ymail.com>
2021-09-12 23:19:00 -0300

    Criando class Checkout...Entity, Resource, Service e Serviceimpl

---
+++ checkout/src/main/java/br/patrickfrr/ecommerce/checkout/service/CheckoutServiceImpl.java
@@ -0,0 +1,50 @@
+package br.patrickfrr.ecommerce.checkout.service;
+
+import br.patrickfrr.ecommerce.checkout.entity.CheckoutEntity;
+import br.patrickfrr.ecommerce.checkout.resource.checkout.CheckoutRequest;
+
+import java.util.Optional;
+
+public class CheckoutServiceImpl { private final CheckoutRepository checkoutRepository;
+    private final CheckoutCreatedSource checkoutCreatedSource;
+    private final UUIDUtil uuidUtil;
+
+    @Override
+    public Optional<CheckoutEntity> create(CheckoutRequest checkoutRequest) {
+        log.info("M=create, checkoutRequest={}", checkoutRequest);
+        final CheckoutEntity checkoutEntity = CheckoutEntity.builder()
+                .code(uuidUtil.createUUID().toString())
+                .status(CheckoutEntity.Status.CREATED)
+                .saveAddress(checkoutRequest.getSaveAddress())
+                .saveInformation(checkoutRequest.getSaveInfo())
+                .shipping(ShippingEntity.builder()
+                        .address(checkoutRequest.getAddress())
+                        .complement(checkoutRequest.getComplement())
+                        .country(checkoutRequest.getCountry())
+                        .state(checkoutRequest.getState())
+                        .cep(checkoutRequest.getCep())
+                        .build())
+                .build();
+        checkoutEntity.setItems(checkoutRequest.getProducts()
+                .stream()
+                .map(product -> CheckoutItemEntity.builder()
+                        .checkout(checkoutEntity)
+                        .product(product)
+                        .build())
+                .collect(Collectors.toList()));
+        final CheckoutEntity entity = checkoutRepository.save(checkoutEntity);
+        final CheckoutCreatedEvent checkoutCreatedEvent = CheckoutCreatedEvent.newBuilder()
+                .setCheckoutCode(entity.getCode())
+                .setStatus(entity.getStatus().name())
+                .build();
+        checkoutCreatedSource.output().send(MessageBuilder.withPayload(checkoutCreatedEvent).build());
+        return Optional.of(entity);
+    }
+
+    @Override
+    public Optional<CheckoutEntity> updateStatus(String checkoutCode, CheckoutEntity.Status status) {
+        final CheckoutEntity checkoutEntity = checkoutRepository.findByCode(checkoutCode).orElse(CheckoutEntity.builder().build());
+        checkoutEntity.setStatus(CheckoutEntity.Status.APPROVED);
+        return Optional.of(checkoutRepository.save(checkoutEntity));
+    }
+}
